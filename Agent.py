e95b8dd7dbb61939ddb2d598f66739bcce582021
Erfan <erfan@Hassans-Air.byod.gmu.edu>
2018-06-14 10:15:15 -0400

    work at home on search tree maker

--- Agent.py
+++ Agent.py
@@ -1,3 +1,40 @@
+
+class Institute(object):
+    def __init__(self, name, boxes):
+        self.name = name
+        self.boxes = boxes
+
+    def regen_boxes(self): #  removes the opened box from the coice of boxes
+        pass
+
+
+
+class Agent(object):
+    def __init__(self, name, boxes):
+        self.name = name
+        self.boxes = boxes
+        self.choices = []
+
+    def gen_utilities(self):
+        for box in self.boxes:
+            p = box[1] #  p is the probablity
+            r = box[2] #  r is the reward in the box
+            c = 5 ###  To Do: generate c with code
+            reservation_price = (p * r - c) / p
+            self.choices.append((box[0],reservation_price))
+        self.choices = dict(self.choices)
+
+    def choose_box(self):
+        return max(self.choices, key =self.choices.get)
+
+
+
+
+
+
+
+
+
 e1 = Environment('first_experiment')
 e1.reward(100)
 e1.set_box(5)
--- Environment.py
+++ Environment.py
@@ -11,37 +11,19 @@ class Environment(object):
     def set_box(self, number):   # number of boxes to generate
 
         for i in range(0, number):
-            p = rnd.random()      # p is the probablity of successfully getting the reward inside the box
-            self.box.append(('box_' + str(1+i) , p, self.reward))
+            p = rnd.random()      # p is the probability of successfully getting the reward inside the box
+            self.box.append(('box_' + str(1+i), p, self.reward))
+        self.number = number
 
+    def create_box_tree(self, name_of_the_file):
+        n = self.number
+        file = open(name_of_the_file, "w+")
+        file.write("nice bro!!!!!!")
+        print (n)
 
-class Institute(object):
-    def __init__(self, name, boxes):
-        self.name = name
-        self.boxes = boxes
-
-    def regen_boxes(self): #  removes the opened box from the coice of boxes
-        pass
-
-
-
-class Agent(object):
-    def __init__(self, name, boxes):
-        self.name = name
-        self.boxes = boxes
-        self.choices = []
-
-    def gen_utilities(self):
-        for box in self.boxes:
-            p = box[1] #  p is the probablity
-            r = box[2] #  r is the reward in the box
-            c = 5 ###  To Do: generate c with code
-            reservation_price = (p * r - c) / p
-            self.choices.append((box[0],reservation_price))
-        self.choices = dict(self.choices)
-
-    def choose_box(self):
-        return max(self.choices, key =self.choices.get)
+    def create_nod(self,a):
+        a = 12
+        return a
 
 
 
@@ -49,11 +31,9 @@ e1 = Environment('first_experiment')
 e1.reward(100)
 e1.set_box(5)
 print(e1.box)
-a1 = Agent('Adam', e1.box)
-a1.gen_utilities()
-print(a1.choices)
-Choice = a1.choose_box()
-print(Choice)
+e1.create_box_tree("tree_file.txt")
+
+
 
 
 
